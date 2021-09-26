20d5d97da7ecce96cafc0ba4e2dbd3505ae9fd2b
Yosef-S-A <yosefsamuel22@gmail.com>
2021-09-26 18:42:13 +0000

    task 1, 2 route

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import uuid
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,12 +8,9 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-port = 5000
-host = '0.0.0.0'
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -21,20 +19,32 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb')
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
-    """ Home page that handles popdown with states,amenities and places"""
+    """ HBNB is alive! """
     states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
     amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
     places = storage.all(Place).values()
-    cache_id = uuid.uuid4()
-    return render_template('0-hbnb.html',
+    places = sorted(places, key=lambda k: k.name)
+
+    cache_id = str(uuid.uuid4())
+
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
                            cache_id=cache_id)
 
 
+
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=5000)
